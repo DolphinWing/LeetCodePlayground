054f06b9fc4fa74cc8e5c3a3a30942544cc47b0b
Jimmy Hu <Jimmy.Hu@avermedia.com>
2020-07-20 01:53:22 +0800

    Merge Two Sorted Lists

--- app/src/main/java/dolphin/android/apps/leetcodeplayground/Solution.kt
+++ app/src/main/java/dolphin/android/apps/leetcodeplayground/Solution.kt
@@ -11,4 +11,33 @@ class Solution : AcceptedSolution() {
         Log.d(TAG, "Here's the work in progress codes")
         return true
     }
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    /**
+     * https://leetcode.com/problems/merge-two-sorted-lists/
+     */
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        val list = ArrayList<ListNode>()
+        var next: ListNode? = l1
+        while (next != null) { // add l1 to a list for sorting
+            list.add(next)
+            next = next.next
+        }
+        next = l2 // add all l2 to a list for sorting
+        while (next != null) {
+            list.add(next)
+            next = next.next
+        }
+        list.sortBy { it.`val` } // sort by index
+        return if (list.isNotEmpty()) {
+            list.last().next = null // mark last has no next
+            for (i in 0 until list.lastIndex) {
+                list[i].next = list[i + 1]
+            }
+            list.first() // return the first one
+        } else null // no list
+    }
 }
--- app/src/test/java/dolphin/android/apps/leetcodeplayground/LeetCodeTest.kt
+++ app/src/test/java/dolphin/android/apps/leetcodeplayground/LeetCodeTest.kt
@@ -1,6 +1,8 @@
 package dolphin.android.apps.leetcodeplayground
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -19,5 +21,85 @@ class LeetCodeTest {
     fun wip() {
         assertTrue(solution.wip())
         assertEquals(3 + 4, 4 + 3)
+    }
+
+    @Test
+    fun `Merge Two Sorted Lists 1`() {
+        val l1 = Solution.ListNode(1).apply {
+            next = Solution.ListNode(2).apply {
+                next = Solution.ListNode(4)
+            }
+        }
+        val l2 = Solution.ListNode(1).apply {
+            next = Solution.ListNode(3).apply {
+                next = Solution.ListNode(4)
+            }
+        }
+        val result = solution.mergeTwoLists(l1, l2)
+        assertNotNull(result)
+        assertEquals(1, result!!.`val`)
+        assertNotNull(result.next)
+        result.next!!.let { n1 ->
+            assertEquals(1, n1.`val`)
+            assertNotNull(n1.next)
+            n1.next!!.let { n2 ->
+                assertEquals(2, n2.`val`)
+                assertNotNull(n2.next)
+                n2.next!!.let { n3 ->
+                    assertEquals(3, n3.`val`)
+                    assertNotNull(n3.next)
+                    n3.next!!.let { n4 ->
+                        assertEquals(4, n4.`val`)
+                        assertNotNull(n4.next)
+                        n4.next!!.let { n5 ->
+                            assertEquals(4, n5.`val`)
+                            assertNull(n5.next)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Merge Two Sorted Lists 2`() {
+        val l1 = Solution.ListNode(1).apply {
+            next = Solution.ListNode(2).apply {
+                next = Solution.ListNode(4)
+            }
+        }
+        val result = solution.mergeTwoLists(l1, null)
+        assertNotNull(result)
+        assertEquals(1, result!!.`val`)
+        assertNotNull(result.next)
+        result.next!!.let { n1 ->
+            assertEquals(2, n1.`val`)
+            assertNotNull(n1.next)
+            n1.next!!.let { n2 ->
+                assertEquals(4, n2.`val`)
+                assertNull(n2.next)
+            }
+        }
+    }
+
+    @Test
+    fun `Merge Two Sorted Lists 3`() {
+        val l2 = Solution.ListNode(1).apply {
+            next = Solution.ListNode(3).apply {
+                next = Solution.ListNode(4)
+            }
+        }
+        val result = solution.mergeTwoLists(null, l2)
+        assertNotNull(result)
+        assertEquals(1, result!!.`val`)
+        assertNotNull(result.next)
+        result.next!!.let { n1 ->
+            assertEquals(3, n1.`val`)
+            assertNotNull(n1.next)
+            n1.next!!.let { n2 ->
+                assertEquals(4, n2.`val`)
+                assertNull(n2.next)
+            }
+        }
     }
 }
